be196cd408e226c16f8c93cc980fbb38fdcd7e26
Bernardo112 <bernardo.cruz@etec.sp.gov.br>
2023-03-22 03:15:24 +0900

    Building the Calculo Estacionamento screen

---
+++ www/components/loom.js
@@ -0,0 +1,7 @@
+// This is a JavaScript file
+
+function contaEstacionamento(){
+    var placa = document.getElementById("placa");
+    var categoria = document.getElementById("categoria");
+    var qHoras = document.getElementById("qHoras");
+}
