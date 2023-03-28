85b6ff76b1159c6175dfab2e3383cb0af57057d5
Bernardo112 <bernardo.cruz@etec.sp.gov.br>
2023-03-28 12:39:14 +0900

    Result function is now working

--- www/components/loom.js
+++ www/components/loom.js
@@ -1,17 +1,85 @@
-// This is a JavaScript file
+var resultado = "";
 
 function ContaEstacionamento(){
-    var placa = document.getElementById("placa");
-    var categoria = document.getElementById("categoria");
-    var qHoras = document.getElementById("qHoras");
+    var placa = document.getElementById("placa").value;
+    var categoria = document.getElementById("categoria").value;
+    var qHoras = document.getElementById("qHoras").value;
 
-    alert("Placa inválida, tente novamente");
-    /*if(placa.length() > 7){
-        alert("Placa inválida, tente novamente");
-        document.getElementById("placa").innerHTML = "";
-        document.getElementById("categoria").innerHTML = "";
-        document.getElementById("qHoras").innerHTML = "";
-    }*/
+    if(categoria == "Selecione" || qHoras == "Selecione"){
+        resultado = "Preencha todos os campos";
+        return;
+    }
+
+    if(placa.length > 7 || placa.length < 7){
+        resultado = "Placa inválida, tente novamente";
+        return;
+    }
+
+    for (var i = 0; i < placa.length; i++) {
+        var char1 = placa.charAt(i);
+        var cc = char1.charCodeAt(0);
+
+        if ((cc > 47 && cc < 58) || (cc > 64 && cc < 91) || (cc > 96 && cc < 123)) {
+        } else {
+            resultado = "Somente caracteres alfanuméricos";
+            return;
+        }
+    }
+    
+    for (var i = 0; i < placa.length; i++) {
+        var char1 = placa.charAt(i);
+        var cc = char1.charCodeAt(0);
+
+        if(i < 2){
+            if ((cc > 64 && cc < 91) || (cc > 96 && cc < 123)) {
+            } else {
+            resultado = "Placa inválida, tente novamente";
+            return;
+            }
+        }
+        if(i > 2 && i != 4){
+            if ((cc > 47 && cc < 58)) {
+            } else {
+            resultado = "Placa inválida, tente novamente";
+            return;
+            }
+        }
+    }
+    if(resultado == ""){
+        resultado = "O valor da compra do estacionamento para o carro de placa " + placa + " na categoria " + categoria + " é " + (qHoras * PrecoCategoria(categoria));
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
+    document.getElementById("botao").innerHTML = "Calcular novamente";
+    document.getElementById("botao").onclick = "RestaurarCalculadora()";
+}
+
+function RestaurarCalculadora(){
+    document.getElementById("botao").onclick = "ContaEstacionamento()";
+    document.getElementById("resultado").innerHTML = "";
+}
+
+function PrecoCategoria(categoria){
+    switch (categoria){
+    case "subcompacto":
+        return 3.50;
+    break;
+    case "compacto":
+        return 4.00;
+    break;
+    case "hatch":
+        return 4.50;
+    break;
+    case "sedan":
+        return 5.00;
+    break;
+    case "suv":
+        return 6.00;
+    break;
+    case "pickUp":
+        return 7.00;
+    break;
+    }
 }
 
 function Login(event) {
